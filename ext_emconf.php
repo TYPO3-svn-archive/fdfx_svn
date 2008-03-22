b8b954bd6803217682bdebb0d80869750c228ac5
petruss <petruss@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-03-22 14:20:09 +0000

    Initial import.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/fdfx_svn/trunk@8675 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,46 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "fdfx_svn"
+#
+# Auto generated 13-07-2007 09:18
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => '4Many CVS/SVN Updater',
+	'description' => 'Loads the latest version from a CVS/SVN. To do so a script has to be setup. ONLY for developers! No manual as there is no need :-)',
+	'category' => 'module',
+	'author' => 'Peter Russ',
+	'author_email' => 'peter.russ@4many.net',
+	'shy' => 1,
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => 'mod1',
+	'state' => 'stable',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '4Many Services',
+	'version' => '1.0.0',
+	'_md5_values_when_last_written' => 'a:13:{s:9:"ChangeLog";s:4:"41da";s:10:"README.txt";s:4:"8b31";s:21:"ext_conf_template.txt";s:4:"a09f";s:12:"ext_icon.gif";s:4:"021e";s:14:"ext_tables.php";s:4:"8229";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"dbce";s:14:"mod1/index.php";s:4:"6260";s:18:"mod1/locallang.php";s:4:"9ffa";s:22:"mod1/locallang_mod.php";s:4:"e7ee";s:19:"mod1/moduleicon.gif";s:4:"021e";s:19:"doc/wizard_form.dat";s:4:"48aa";s:20:"doc/wizard_form.html";s:4:"9142";}',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'suggests' => array(
+	),
+);
+
+?>
